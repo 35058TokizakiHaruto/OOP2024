fc66ba036177d3b422506ff8ef5f54d4d1d9de33
TokizakiHaruto <166583718+35058TokizakiHaruto@users.noreply.github.com>
2024-04-18 14:19:00 +0900

    計算ロジックをクラスとして分離

--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -1,40 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DistanceConverter {
     internal class Program {
         static void Main(string[] args) {
-            if(args.Length >= 1 && args[0] =="-tom") {
+            if (args.Length >= 1 && args[0] == "-tom") {
                 //フィートからメートルへの対応表を出力
+                PrintFeetToMeterList(int.Parse(args[1]), int.Parse(args[2]));
             } else {
                 //メートルからフィートへの対応表を出力
-               
-                }
+                PrintfeetMeterToList(int.Parse(args[1]), int.Parse(args[2]));
             }
-            private static void PrintfeetToMeterList(int start, int stop) {
-                for (int feet = 1; feet <= 10; feet++) {
-                    double meter = FeetToMeter(feet);
-                    Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
-                }
-            }
-            private static void PrintfeetMeterToList(int start, int stop) {
-            for (int meter = 1; meter <= 10; meter++) {
-                double feet = FeetMeterTo(meter);
-                Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
-
-
-
-                //フィートからメートルを求める
-                static double FeetToMeter(int feet) {
-            return feet * 0.3048;
         }
-        //メートルからフィートを求める
-        static double MeterToFeet(int meter) {
-            return meter * 0.3048;
+        private static void PrintFeetToMeterList(int start, int stop) {
+            FeetConverter fc = new FeetConverter();
+
+            for (int feet = start; feet <= stop; feet++) {
+
+                double meter = fc.ToMeter(feet);
+                Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+            }
+        }
+        private static void PrintfeetMeterToList(int start, int stop) {
+            FeetConverter fc = new FeetConverter();
+            for (int meter = start; meter <= stop; meter++) {
+                double feet = fc.FromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
+            }
         }
     }
 }
+

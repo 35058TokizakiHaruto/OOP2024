73d442723b1d664037e2e14430e995a6fc8b210f
TokizakiHaruto <166583718+35058TokizakiHaruto@users.noreply.github.com>
2024-04-24 10:54:29 +0900

    売上集計プログラム新規追加

---
+++ Chapter02/SalesCounter/Sale.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCounter {
+    public class Sale {
+        //商品名
+        public string ShopName { get; set; }
+
+        //商品カテゴリ
+        public string ProductcCategory { get; set; }
+
+        //売上高
+        public int Amount { get; set; }
+    }
+}

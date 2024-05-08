28569c979182fc8358b085b4cd1a0825eb5b6d7b
TokizakiHaruto <166583718+35058TokizakiHaruto@users.noreply.github.com>
2024-05-08 10:16:16 +0900

    跳ね返り処理実装

---
+++ Chapter02/演習問題/Exercise2'/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2_ {
+    internal class Program {
+        static void Main(string[] args) {
+            if (args.Length >= 1 && args[0] == "-tom") {
+                //ヤードからメートルへの対応表を出力
+                PrintYardToMeterList(1, 10);
+            } else {
+                //メートルからヤードへの対応表を出力
+                PrintyardMeterToList(int.Parse(args[1]), int.Parse(args[2]));
+            }
+        }
+        private static void PrintYdToMeterList(int start, int stop) {
+            for (int yd = start; yd <= stop; yd++) {
+                double meter = yardConverter.ToMeter(yd);
+                Console.WriteLine("{0} it = {1:0.0000} m", yd, meter);
+            }
+        }
+        private static void PrintydMeterToList(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double yd = YardConverter.FromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} it", meter, yd);
+            }
+        }
+    }
+}
---
+++ Chapter02/演習問題/Exercise2'/YardConveter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    internal class YardConveter {
+        //private const double ratio = 0.91440; 　//定数
+        public static readonly double ratio = 0.91440; //定数(外部にも公開する場合)
+
+        //メートルからヤードを求める
+        public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+
+        //ヤードからメートルを求める
+        public static double ToMeter(double yd) {
+            return yd * ratio;
+        }
+    }
+}

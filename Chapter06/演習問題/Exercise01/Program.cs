1c524afed22798e6fd657cb336481876b99d7385
TokizakiHaruto <166583718+35058TokizakiHaruto@users.noreply.github.com>
2024-06-05 13:55:18 +0900

    6.1.5完成

--- Chapter06/演習問題/Exercise01/Program.cs
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -22,23 +22,38 @@ namespace Exercise01 {
             Exercise1_4(numbers);
             Console.WriteLine("-----");
 
-            throw new NotImplementedException();
+            Exercise1_5(numbers);
+            Console.WriteLine("-----");
         }
 
+
         private static void Exercise1_1(int[] numbers) {
-           var Max = numbers.Max();
+            var Max = numbers.Max();
+            Console.WriteLine(Max.ToString());
         }
 
         private static void Exercise1_2(int[] numbers) {
-            
-        }
+            int[] last2 = numbers.Skip(numbers.Length - 2).ToArray();
+            foreach (var x in last2) {
+                Console.WriteLine(x);
+            }
 
+        }
         private static void Exercise1_3(int[] numbers) {
-           
+            string[] result = Array.ConvertAll(numbers, x => x.ToString());
+            Console.WriteLine(String.Join(",", result));
         }
 
         private static void Exercise1_4(int[] numbers) {
-            
+            var sortenumbers = numbers.OrderBy(x => x).Take(3).ToArray();
+            foreach (var x in sortenumbers) {
+                Console.WriteLine(x.ToString());
+            }
+        }
+        private static void Exercise1_5(int[] numbers) {
+            var results = numbers.Distinct().ToList();
+            int intCount = results.Count(x => 10 < x);
+            Console.WriteLine(intCount);
         }
     }
 }

0f1dfd8cd234d0831d3552d2b7c16e4a8dc83824
TokizakiHaruto <166583718+35058TokizakiHaruto@users.noreply.github.com>
2024-06-11 15:52:49 +0900

    都道府県、県庁所在地プログラム完成

---
+++ Chapter07/Employee/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+        namespace Section01 {
+        internal class Employee {
+            public string Name { get; set; }
+            public int Id { get; set; }
+
+            public Employee(int id, string name) {
+                Id = id;
+                Name = name;
+
+            }
+        }
+    }
+
+

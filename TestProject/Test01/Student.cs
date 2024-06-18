f949a260e06a3c2784e4c027528617c93628a36c
TokizakiHaruto <166583718+35058TokizakiHaruto@users.noreply.github.com>
2024-06-18 11:05:17 +0900

    設問1_Studentクラス完成

---
+++ TestProject/Test01/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    class Student {
+        //学生の名前
+        public string Name { get; set; }
+
+        //科目名
+        public string Subject { get; set; }
+
+        //点数
+        public int Score { get; set; }
+
+    }
+}

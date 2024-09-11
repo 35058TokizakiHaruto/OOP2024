6141d65d65ea381a9c71ad1503929bd34d7f4214
TokizakiHaruto <166583718+35058TokizakiHaruto@users.noreply.github.com>
2024-09-11 14:52:13 +0900

    データベース接続文字列の確認まで

--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -13,20 +13,35 @@ using System.Xml.Linq;
 namespace RssReader {
     public partial class Form1 : Form {
         List<RssItem> items;
+        Dictionary<string, string> rssDict;
         public Form1() {
+
             InitializeComponent();
+
+            rssDict = new Dictionary<string, string>() {
+                 { "主要", "https://news.yahoo.co.jp/rss/topics/top-picks.xml" },
+                 { "国内", "https://news.yahoo.co.jp/rss/topics/domestic.xml" },
+                 { "国際", "https://news.yahoo.co.jp/rss/topics/world.xml" },
+                 { "経済", "https://news.yahoo.co.jp/rss/topics/business.xml" },
+                 { "エンタメ", "https://news.yahoo.co.jp/rss/topics/business.xml" },
+                 { "スポーツ", "https://news.yahoo.co.jp/rss/topics/sports.xml" },
+                 { "IT", "https://news.yahoo.co.jp/rss/topics/it.xml" },
+                 { "科学", "https://news.yahoo.co.jp/rss/topics/science.xml" },
+                 { "地域", "https://news.yahoo.co.jp/rss/topics/local.xml" },
+                };
         }
+
 
         private void btGet_Click(object sender, EventArgs e) {
             using (var wc = new WebClient()) {
                 var url = wc.OpenRead(Rss_ComboBox.Text);
                 var xdoc = XDocument.Load(url);
 
-                var items = xdoc.Root.Descendants("item")
-                                    .Select(item => new RssItem {
-                                        Title = item.Element("title").Value,
-                                        Link = item.Element("link").Value,
-                                    });
+                items = xdoc.Root.Descendants("item")
+                                   .Select(item => new RssItem {
+                                       Title = item.Element("title").Value,
+                                       Link = item.Element("link").Value,
+                                   }).ToList();
 
                 foreach (var title in items) {
                     lbRssTitle.Items.Add(title.Title);
@@ -58,25 +73,16 @@ namespace RssReader {
                         if (selectedItem != null) {
                             if (!string.IsNullOrEmpty(selectedItem.Link)) {
                                 webView21.Source = new Uri(selectedItem.Link);
-                            } else {
-                                MessageBox.Show("Selected item link is null or empty.");
                             }
-                        } else {
-                            MessageBox.Show("Selected item not found.");
                         }
-                    } else {
-                        MessageBox.Show("Items collection is not initialized.");
                     }
                 }
+
             }
             catch (Exception ex) {
                 MessageBox.Show($"An error occurred: {ex.Message}");
             }
         }
-
-
-
-
     }
 }
 //データ格納用のクラス

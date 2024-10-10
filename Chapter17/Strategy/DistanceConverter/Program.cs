620f2ece43e0cc16468d4de29bc519514d9c9acf
TokizakiHaruto <166583718+35058TokizakiHaruto@users.noreply.github.com>
2024-10-10 10:18:18 +0900

    p429まで終わり DistanceConverterのProgramクラス

--- Chapter17/Strategy/DistanceConverter/Program.cs
+++ Chapter17/Strategy/DistanceConverter/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DistanceConverter {
-    internal class Program {
+    class Program {
         static void Main(string[] args) {
+            while (true) {
+                var from = GetConverter("変換元の単位を入力してください");
+                var to = GetConverter("変換先の単位を入力してください");
+                var distance = GetDistance(from);   //変換したい距離を入力
+
+                var Converter = new DistanceConverter(from, to);
+                var result = Converter.Converter(distance);
+                Console.WriteLine($"{distance}{from.UnitName}は{result:0.000}{to.UnitName}です\n");
+            }
+        }
+        //変換したい距離を入力
+        static double GetDistance(ConverterBase from) {
+            double? value = null;
+            do {
+                Console.Write($"変換したい距離(単位:{from.UnitName}を入力してください => ");
+                var line = Console.ReadLine();
+                double temp;
+                value = double.TryParse(line, out temp) ? (double?)temp : null;
+            } while (value == null);
+            return value.Value;
+        }
+
+        static ConverterBase GetConverter(string msg) {
+            ConverterBase converter = null;
+            do {
+                Console.Write(msg + " => ");
+                var unit = Console.ReadLine();
+                converter = ConverterFactory.GetInstance(unit);
+            } while (converter == null);
+            return converter;
         }
     }
 }
